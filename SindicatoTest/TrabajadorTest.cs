36ca0b9fb558c4a26933bdf66fd0e5dd6a84ddf0
The0Torres <theotocar@gmail.com>
2023-11-18 17:01:47 -0300

    ordenar

--- SindicatoTest/TrabajadorTest.cs
+++ SindicatoTest/TrabajadorTest.cs
@@ -35,5 +35,23 @@ namespace SindicatoTest
             Assert.IsFalse(sindicato.Trabajadores.Contains(periodista));
         }
 
+        [TestMethod]
+        public void TestOrdenarPorNombreAscendente()
+        {
+            // Arrange
+            Sindicato sindicato = new Sindicato();
+            Periodista periodista1 = new Periodista("Zeta", "Apellido", 25, ETipo.Tiempocompleto, "Deportes", 90, EMedios.Television);
+            Periodista periodista2 = new Periodista("Alfa", "Apellido", 30, ETipo.Tiempocompleto, "Política", 85, EMedios.Radio);
+            sindicato += periodista1;
+            sindicato += periodista2;
+
+            // Act
+            sindicato.OrdenarPorNombreAscendente();
+
+            // Assert
+            Assert.AreEqual("Alfa", sindicato.Trabajadores[0].Nombre);
+            Assert.AreEqual("Zeta", sindicato.Trabajadores[1].Nombre);
+        }
+
     }
 }
