f85a183fc0c6f57a6dd11f968dc23e798e070eb9
The0Torres <theotocar@gmail.com>
2023-11-18 16:58:16 -0300

    test agregar periodista

--- SindicatoTest/TrabajadorTest.cs
+++ SindicatoTest/TrabajadorTest.cs
@@ -1,11 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Laburos;
+
 namespace SindicatoTest
 {
     [TestClass]
     public class TrabajadorTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAgregarPeriodista()
         {
+            // Arrange
+            Sindicato sindicato = new Sindicato();
+            Periodista periodista = new Periodista("Nombre", "Apellido", 25, ETipo.Tiempocompleto, "Deportes", 90, EMedios.Television);
+
+            // Act
+            sindicato += periodista;
+
+            // Assert
+            Assert.IsTrue(sindicato.Trabajadores.Contains(periodista));
         }
+
     }
 }
