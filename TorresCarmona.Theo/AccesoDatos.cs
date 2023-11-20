8aa2e5b72add516e1bdf3940efbbd92a4ef9b0a1
The0Torres <theotocar@gmail.com>
2023-11-20 00:32:29 -0300

    funcion modificar

--- TorresCarmona.Theo/AccesoDatos.cs
+++ TorresCarmona.Theo/AccesoDatos.cs
@@ -187,7 +187,7 @@ namespace Laburos
             }
             catch (Exception ex)
             {
-
+               
             }
             finally
             {
@@ -275,5 +275,133 @@ namespace Laburos
 
             return retorno;
         }
+
+        public bool ModificarPeriodista(Periodista periodista)
+        {
+            bool retorno = false;
+
+            try
+            {
+                this.comando = new SqlCommand();
+                this.comando.Parameters.Clear();
+                this.comando.Parameters.AddWithValue("@nombre", periodista.Nombre);
+                this.comando.Parameters.AddWithValue("@apellido", periodista.Apellido);
+                this.comando.Parameters.AddWithValue("@salario", (double)periodista.Salario);
+                this.comando.Parameters.AddWithValue("@tipo", (int)periodista.Tipo);
+                this.comando.Parameters.AddWithValue("@id", (int)periodista.Id);
+                this.comando.Parameters.AddWithValue("@especializacion", periodista.Especializacion);
+                this.comando.Parameters.AddWithValue("@credibilidad", (double)periodista.Credibilidad);
+                this.comando.Parameters.AddWithValue("@medio", (int)periodista.Medio);
+
+                this.comando.CommandType = System.Data.CommandType.Text;
+                this.comando.CommandText = "UPDATE Periodista SET nombre = @nombre, apellido = @apellido, salario = @salario, tipo = @tipo, especializacion = @especializacion, credibilidad = @credibilidad, medio = @medio WHERE id = @id";
+                this.comando.Connection = this.conexion;
+
+                this.conexion.Open();
+
+                int filasAfectadas = this.comando.ExecuteNonQuery();
+
+                return filasAfectadas == 1;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                if (this.conexion.State == System.Data.ConnectionState.Open)
+                {
+                    this.conexion.Close();
+                }
+            }
+
+            return retorno;
+        }
+
+        public bool ModificarCirujano(Cirujano cirujano)
+        {
+            bool retorno = false;
+
+            try
+            {
+                this.comando = new SqlCommand();
+                this.comando.Parameters.Clear();
+                this.comando.Parameters.AddWithValue("@nombre", cirujano.Nombre);
+                this.comando.Parameters.AddWithValue("@apellido", cirujano.Apellido);
+                this.comando.Parameters.AddWithValue("@salario", (double)cirujano.Salario);
+                this.comando.Parameters.AddWithValue("@tipo", (int)cirujano.Tipo);
+                this.comando.Parameters.AddWithValue("@id", (int)cirujano.Id);
+                this.comando.Parameters.AddWithValue("@especialidad", cirujano.Especialidad);
+                this.comando.Parameters.AddWithValue("@hospital", (int)cirujano.Hospital);
+                this.comando.Parameters.AddWithValue("@cirugias", cirujano.Cirugias);
+
+                this.comando.CommandType = System.Data.CommandType.Text;
+                this.comando.CommandText = "UPDATE Cirujano SET nombre = @nombre, apellido = @apellido, salario = @salario, tipo = @tipo, especialidad = @especialidad, hospital = @hospital, cirugias = @cirugias WHERE id = @id";
+                this.comando.Connection = this.conexion;
+
+                this.conexion.Open();
+
+                int filasAfectadas = this.comando.ExecuteNonQuery();
+
+                return filasAfectadas == 1;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                if (this.conexion.State == System.Data.ConnectionState.Open)
+                {
+                    this.conexion.Close();
+                }
+            }
+
+            return retorno;
+        }
+
+        public bool ModificarDeportista(Deportista deportista)
+        {
+            bool retorno = false;
+
+            try
+            {
+                this.comando = new SqlCommand();
+                this.comando.Parameters.Clear();
+                this.comando.Parameters.AddWithValue("@nombre", deportista.Nombre);
+                this.comando.Parameters.AddWithValue("@apellido", deportista.Apellido);
+                this.comando.Parameters.AddWithValue("@salario", (double)deportista.Salario);
+                this.comando.Parameters.AddWithValue("@tipo", (int)deportista.Tipo);
+                this.comando.Parameters.AddWithValue("@id", (int)deportista.Id);
+                this.comando.Parameters.AddWithValue("@deporte", deportista.Deporte);
+                this.comando.Parameters.AddWithValue("@trofeos", deportista.Trofeos);
+                this.comando.Parameters.AddWithValue("@rankingMundial", deportista.RankingMundial);
+
+                this.comando.CommandType = System.Data.CommandType.Text;
+                this.comando.CommandText = "UPDATE Deportista SET nombre = @nombre, apellido = @apellido, salario = @salario, tipo = @tipo, deporte = @deporte, trofeos = @trofeos, rankingMundial = @rankingMundial WHERE id = @id";
+                this.comando.Connection = this.conexion;
+
+                this.conexion.Open();
+
+                int filasAfectadas = this.comando.ExecuteNonQuery();
+
+                return filasAfectadas == 1;
+            }
+            catch (Exception ex)
+            {
+  
+            }
+            finally
+            {
+                if (this.conexion.State == System.Data.ConnectionState.Open)
+                {
+                    this.conexion.Close();
+                }
+            }
+
+            return retorno;
+        }
     }
+
+
 }
